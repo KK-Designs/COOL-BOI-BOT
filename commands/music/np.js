3bceddb23ea9c3dd45db8325938b580144982f49
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2021-09-13 12:06:50 -1000

    Fixed bar for np command

--- commands/music/np.js
+++ commands/music/np.js
@@ -1,6 +1,6 @@
 const {MessageEmbed} = require('discord.js');
 const humanizeDuration = require("humanize-duration");
-const Bar = require("string-progressbar")
+const Bar = require("string-progressbar");
 module.exports = {
   name: 'np',
   description: 'See the surrent dong playing.',
@@ -15,14 +15,23 @@ module.exports = {
     if (!serverQueue)
       return message.channel.send('There is nothing playing.');
 
+    const currentSong = serverQueue.songs[0];
+    const currentDuration = Math.round(serverQueue.player.state.resource.playbackDuration / 1000);
+    const totalDuration = currentSong.duration;
+    const [bar, percent] = Bar.splitBar(totalDuration, currentDuration, 25);
     const embed = new MessageEmbed()
       .setColor(message.guild.me.displayHexColor)
       .setTitle(`Current Song playing`)
-      .addField(`<:playing:813209288100151366> Now playing:`, `${serverQueue.songs[0].title} • ${serverQueue.songs[0].author}`, true)     //.addField(`Duration:`, serverQueue.songs.map(song => `${humanizeDuration(song.duration * 1000)}`), true)
-      .addField(`Duration:`, `${humanizeDuration(serverQueue.songs[0].duration * 1000)}`, true)
+      .addField(
+        `<:playing:813209288100151366> Now playing:`,
+        `${currentSong.title} • ${currentSong.author}`,
+        true
+      )
+      .addField(`Duration:`, `${humanizeDuration(totalDuration * 1000)}`, true)
+      .addField(`\u200b`, bar)
       .setTimestamp()
       .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
-
+  
     return message.reply({embeds: [embed]});
   },
   async executeSlash(interaction) {
@@ -32,9 +41,9 @@ module.exports = {
       return interaction.channel.send('There is nothing playing.');
 
     const currentSong = serverQueue.songs[0];
-    const currentDuration = serverQueue.player.state.resource.playbackDuration
-    const totalDuration = currentSong.duration * 1000;
-    const bar = Bar.splitBar(totalDuration, currentDuration, 20).join("")
+    const currentDuration = Math.round(serverQueue.player.state.resource.playbackDuration / 1000);
+    const totalDuration = currentSong.duration;
+    const [bar, percent] = Bar.splitBar(totalDuration, currentDuration, 25);
     const embed = new MessageEmbed()
       .setColor(interaction.guild.me.displayHexColor)
       .setTitle(`Current Song playing`)
@@ -44,8 +53,8 @@ module.exports = {
         true
       )
       //.addField(`Duration:`, serverQueue.songs.map(song => `${humanizeDuration(song.duration * 1000)}`), true)
-      .addField(`Duration:`, `${humanizeDuration(totalDuration)}`, true)
-      .addField(`Bar:`, bar, true)
+      .addField(`Duration:`, `${humanizeDuration(totalDuration * 1000)}`, true)
+      .addField(`\u200b`, bar)
       .setTimestamp()
       .setFooter(interaction.user.username, interaction.user.displayAvatarURL({dynamic: true}));
 
