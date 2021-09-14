3d736b69721099ed39a31d8d35521dd5d840c1d8
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2021-09-13 16:28:56 -1000

    Fixed joke command

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -1,58 +1,78 @@
+const {MessageEmbed} = require('discord.js');
+const {spoiler, quote, blockQuote} = require('@discordjs/builders');
+const fetch = require("node-fetch").default;
+const color = require('../../color.json');
 module.exports = {
   name: 'joke',
   description: 'Gives a funny joke 😆',
   aliases: ['pun'],
   usage: '(joke id)',
-  	cooldown: 3,
-  	category: 'fun',
-  execute(message, args) {
-    require('dotenv').config();
-    const {MessageEmbed} = require('discord.js');
-    const {spoiler, quote, blockQuote} = require('@discordjs/builders');
-    const color = require('../../color.json');
+  cooldown: 3,
+  category: 'fun',
+  async execute(message, args) {
     const id = parseInt(args[0]);
-    const {Client} = require("blague.xyz");
-    const joker = new Client(process.env.JOKE_API, {
-      defaultLang: "en"
-    });
 
     if (!args[0]) {
-      joker.randomJoke().then((joke) => {
-        const embed = new MessageEmbed()
-          .setTitle(`😆 ${joke.question}`)
-          .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL({dynamic: true})}`)
-          .setColor(color.bot_theme)
-          .setDescription(`${spoiler(joke.answer)}`)
-          .setFooter(`ID: ${joke.id} | The COOL BOI BOT`)
-          .setTimestamp();
+      const joke = await getRandomJoke();
+      const embed = new MessageEmbed()
+        .setTitle(`😆 ${joke.question}`)
+        .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL({dynamic: true})}`)
+        .setColor(color.bot_theme)
+        .setDescription(`${spoiler(joke.answer)}`)
+        .setFooter(`ID: ${joke.id} | The COOL BOI BOT`)
+        .setTimestamp();
 
-        return message.reply({embeds: [embed]});
-      });
-    } else {
-      if (isNaN(id)) {
-        return message.channel.send({embeds: [
-          new MessageEmbed()
-            .setColor('RED')
-            .setDescription('<:X_:807305490160943104> That doesn\'t seem to be a valid number.')
-        ], reply: {messageReference: message.id}});
-      } else if (id < 0 || id > 100) {
-        return message.channel.send({embeds: [
-          new MessageEmbed()
-            .setColor('RED')
-            .setDescription('<:X_:807305490160943104> You need to input a number between 0 and 100.')
-        ], reply: {messageReference: message.id}});
-      }
-      joker.getJoke(id, "en").then((joke) => {
-        const embed = new MessageEmbed()
-          .setTitle(`😆 ${joke.question}`)
-          .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL({dynamic: true})}`)
-          .setColor(color.bot_theme)
-          .setDescription(`${spoiler(joke.answer)}`)
-          .setFooter(`ID: ${id} | The COOL BOI BOT`)
-          .setTimestamp();
-
-        message.reply({embeds: [embed]});
-      });
+      return message.reply({embeds: [embed]});
     }
+    if (isNaN(id)) {
+      return await message.reply({embeds: [
+        new MessageEmbed()
+          .setColor('RED')
+          .setDescription('<:X_:807305490160943104> That doesn\'t seem to be a valid number.')
+      ]});
+    } else if (id < 0 || id > 100) {
+      return await message.reply({embeds: [
+        new MessageEmbed()
+          .setColor('RED')
+          .setDescription('<:X_:807305490160943104> You need to input a number between 0 and 100.')
+      ]});
+    }
+    const joke = await getJokeByID(id);
+    const embed = new MessageEmbed()
+      .setTitle(`😆 ${joke.question}`)
+      .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL({dynamic: true})}`)
+      .setColor(color.bot_theme)
+      .setDescription(`${spoiler(joke.answer)}`)
+      .setFooter(`ID: ${id} | The COOL BOI BOT`)
+      .setTimestamp();
+
+    await message.reply({embeds: [embed]});
   }
 };
+const baseURL = "https://blague.xyz/";
+async function getJoke(path) {
+  const url = new URL(`/api/joke/${path}`, baseURL);
+
+  url.searchParams.set("lang", "EN");
+  const res = await fetch(url);
+
+  if (!res.ok)
+    throw new Error(`HTTP Error ${res.status}: ${res.statusText}`);
+
+  console.log(`${url}`);
+  console.log(res.url);
+  const obj = await res.json();
+
+  if (obj.error) {
+    console.log(obj);
+    throw new Error("The returned object has an error");
+  }
+
+  return obj.joke;
+}
+function getRandomJoke() {
+  return getJoke("random");
+}
+function getJokeByID(id) {
+  return getJoke(`${id}`);
+}
