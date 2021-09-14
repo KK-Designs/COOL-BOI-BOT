8a06dbbb1838de841312f5dea2c26777ee1176dd
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2021-09-13 17:21:04 -1000

    Fixed discordjs command

--- commands/other/discordjs.js
+++ commands/other/discordjs.js
@@ -1,48 +1,62 @@
+const {MessageEmbed} = require('discord.js');
+const {MessageActionRow, MessageButton} = require('discord.js');
 const fetch = require('node-fetch').default;
-const {getPrefix} = require("discord-prefix");
-const sendError = require("../../error.js");
-const {defaultPrefix} = require("../../config.json");
 module.exports = {
   name: 'discordjs',
-  description: 'Gets discord.js v12 docs <:djs:808855899141570591>',
+  description: 'Gets discord.js v13 docs <:djs:882691182692532254>',
   cooldown: 3,
   aliases: ['djs'],
   usage: '[search term]',
   category: 'other',
-  options: {},
   async execute(message, args) {
-    const prefix = await getPrefix(message.guild?.id ?? message.author.id) ?? defaultPrefix;
+    const sendError = require('../../error.js');
 
     if (!args.length)
-      return sendError(`You need to supply search term like \`${prefix}discordjs member\``, message.channel);
+      return sendError('You need to supply search term like `${prefix}discordjs member`', message.channel);
 
     const user = message.author;
     const search = args.join(' ');
-    const url = new URL("/v2/embed", "https://djsdocs.sorta.moe");
+    const res = await fetch(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(search)}`).then(res => res.json())
+      .then(embed => {
+        const deletemsg = new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+              .setCustomId('danger')
+              .setLabel('Delete')
+              .setEmoji('🗑')
+              .setStyle('DANGER')
+          );
 
-    url.searchParams.set("src", "stable");
-    url.searchParams.set("q", search);
-    const res = await fetch(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(search)}`);
+        message.channel.send({embeds: [embed], components: [deletemsg], reply: {messageReference: message.id}}).then(m => {
+          const filter = i => i.customId === 'danger';// && i.user.id === message.author.id;
+          const collector = message.channel.createMessageComponentCollector({filter, time: 15000});
 
-    if (!res.ok)
-      return await sendError(`HTTP Error ${res.status}: ${res.statusText}`);
-    
-    const embed = await res.json();
-    const m = await message.channel.send({embeds: [embed]});
-    const emoji = '🗑';
+          collector.on('collect', async i => {
+            if (i.user.id === message.author.id) {
+              if (i.customId === 'danger') {
+                await i.reply({content: '<:check:807305471282249738> I have deleted the message', ephemeral: true});
+                await m.delete();
+              }
+            } else {
+              await i.reply({content: '<:X_:807305490160943104> These aren\'t your buttons to play around with', ephemeral: true});
+            }
+          });
+          collector.on('end', collected => {
+            const deletemsg1 = new MessageActionRow()
+              .addComponents(
+                new MessageButton()
+                  .setCustomId('danger')
+                  .setLabel('Delete')
+                  .setEmoji('🗑')
+                  .setStyle('DANGER')
+                  .setDisabled(true)
+              );
 
-    await m.react(emoji);
-    const collector = m.createReactionCollector({
-      filter: (r, u) => r.emoji.name === emoji && u.id === message.author.id,
-      max: 1,
-      time: 10000
-    });
+            m.edit({components: [deletemsg1]});
+          });
+        });
+      });
 
-    collector.on('collect', r => {
-      r.message.delete();
-      message.delete();
-      if (message.channel.type !== "DM")
-        m.reactions.removeAll();
-    });
+
   }
 };
