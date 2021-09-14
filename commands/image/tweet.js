8c99aece01b0378a49267f1478188cdfe59eace1
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2021-09-13 17:11:41 -1000

    Removed duplicate tweet command

--- commands/image/tweet.js
+++ commands/image/tweet.js
@@ -1,18 +1,24 @@
+const fetch = require('node-fetch').default;
+const color = require("../../color.json");
+const sendError = require("../../error.js");
+const {MessageEmbed} = require('discord.js');
 module.exports = {
   name: 'tweet',
   description: 'Returns trump tweet!',
   usage: '[message]',
   cooldown: 2,
-  aliases: ['tw', 'ttweet'],
+  aliases: ['tw'],
   category: 'image',
   clientPermissons: 'EMBED_LINKS',
+  options: {
+    message: {
+      type: "String",
+      description: "The message to tweet"
+    }
+  },
   async	execute(message, args) {
     const user = message.author;
-    const fetch = require('node-fetch');
-    const color = require('../../color.json');
-    const sendError = require('../../error.js');
-    const {MessageEmbed} = require('discord.js');
-
+  
     // Get message
     if (!args[0])
       return sendError('Please provide a message to tweet', message.channel);
@@ -21,20 +27,37 @@ module.exports = {
     if (tweet.length >= 68)
       tweet = tweet.slice(0, 65) + '...';
 
-    try {
-      const res = await fetch('https://nekobot.xyz/api/imagegen?type=trumptweet&text=' + tweet);
-      const img = (await res.json()).message;
-      const embed = new MessageEmbed()
-        .setTitle(':flag_us:  Trump Tweet  :flag_us: ')
-        .setImage(img)
-        .setFooter(user.username, user.displayAvatarURL({dynamic: true}))
-        .setTimestamp()
-        .setColor(message.channel.type === 'dm' ? color.discord : message.guild.me.displayHexColor
-        );
+    const res = await fetch('https://nekobot.xyz/api/imagegen?type=trumptweet&text=' + tweet);
+    const img = (await res.json()).message;
+    const embed = new MessageEmbed()
+      .setTitle(':flag_us:  Trump Tweet  :flag_us: ')
+      .setImage(img)
+      .setFooter(user.username, user.displayAvatarURL({dynamic: true}))
+      .setTimestamp()
+      .setColor(message.guild?.me.displayHexColor ?? color.discord);
 
-      message.channel.send({embeds: [embed]});
-    } catch (err) {
-      sendError(`Oops, well this is an error. If this continues dm <@765686109073440808>. \n \nSpeficic error: ${err}`, message.channel);
-    }
+    message.channel.send({embeds: [embed]});
+  },
+  async	executeSlash(interaction) {
+    const user = interaction.user;
+    // Get message
+    let tweet = interaction.options.getString("message", true);
+    if (tweet.length >= 68)
+      tweet = tweet.slice(0, 65) + '...';
+
+    const res = await fetch('https://nekobot.xyz/api/imagegen?type=trumptweet&text=' + tweet);
+
+    if (!res.ok)
+      return await interaction.reply(`HTTP Error ${res.status}: ${res.statusText}`);
+
+    const img = (await res.json()).message;
+    const embed = new MessageEmbed()
+      .setTitle(':flag_us:  Trump Tweet  :flag_us: ')
+      .setImage(img)
+      .setFooter(user.username, user.displayAvatarURL({dynamic: true}))
+      .setTimestamp()
+      .setColor(interaction.guild?.me.displayHexColor ?? color.discord);
+
+    await interaction.reply({embeds: [embed]});
   }
 };
