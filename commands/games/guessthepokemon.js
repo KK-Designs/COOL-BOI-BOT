e57683c390df2eb3368ced691b7dcb8bdaff20b6
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2021-10-05 21:28:53 -1000

    Added slash commands for games

--- commands/games/guessthepokemon.js
+++ commands/games/guessthepokemon.js
@@ -1,24 +1,43 @@
+const color = require('../../color.json');
+const { GuessThePokemon } = require('discord-gamecord');
+const gameOptions = {
+	embed: {
+		title: 'Who\'s This Pokemon?',
+		footer: 'COOL BOI BOT Games',
+		color: color.bot_theme,
+	},
+	time: 60000,
+	othersMessage: '<:X_:807305490160943104> You are not allowed to use buttons for this message!',
+	winMessage: '<:check:807305471282249738> Your guess was correct! The pokemon was **{pokemon}**',
+	stopMessage: '<:check:807305471282249738> I have stopped the game! It was a **{pokemon}** if you were wondering',
+	incorrectMessage: '<:X_:807305490160943104> Your guess was incorrect! The pokemon was **{pokemon}**',
+};
 module.exports = {
 	name: 'guessthepokemon',
 	description: 'Play a game of guess the pokemon!',
 	aliases: ['gtp', 'gp'],
 	cooldown: 5,
 	category: 'games',
+	options: {},
 	execute(message) {
-		const color = require('../../color.json');
-		const { GuessThePokemon } = require('discord-gamecord');
 		new GuessThePokemon({
+			...gameOptions,
 			message: message,
-			embed: {
-				title: 'Who\'s This Pokemon?',
-				footer: `${message.client.user.username} Games`,
-				color: color.bot_theme,
+		}).startGame();
+	},
+	async executeSlash(interaction) {
+		await interaction.deferReply();
+		const message = {
+			author: interaction.user,
+			channel: {
+				async send(option) {
+					return await interaction.editReply(option);
+				},
 			},
-			time: 60000,
-			othersMessage: '<:X_:807305490160943104> You are not allowed to use buttons for this message!',
-			winMessage: '<:check:807305471282249738> Your guess was correct! The pokemon was **{pokemon}**',
-			stopMessage: '<:check:807305471282249738> I have stopped the game! It was a **{pokemon}** if you were wondering',
-			incorrectMessage: '<:X_:807305490160943104> Your guess was incorrect! The pokemon was **{pokemon}**',
+		};
+		new GuessThePokemon({
+			...gameOptions,
+			message: message,
 		}).startGame();
 	},
 };
--- commands/games/trivia.js
+++ commands/games/trivia.js
@@ -1,42 +1,75 @@
+const { Trivia } = require('discord-gamecord');
+const color = require('../../color.json');
+const sendError = require('../../error.js');
+const difficulties = [
+	'easy',
+	'medium',
+	'hard',
+];
+const gameOptions = {
+	embed: {
+		title: 'Trivia',
+		description: '🕓 You have {time} seconds to respond!',
+		color: color.bot_theme,
+	},
+	time: 60000,
+	buttons: {
+		one: '1️⃣',
+		two: '2️⃣',
+		three: '3️⃣',
+		four: '4️⃣',
+	},
+	winMessage: '<:check:807305471282249738> Your answer was correct! You answered **{answer}**',
+	loseMessage: '<:X_:807305490160943104> Your answer was Incorrect! The correct answer was **{answer}**',
+	othersMessage: '<:X_:807305490160943104> You are not allowed to use buttons for this message!',
+};
 module.exports = {
 	name: 'trivia',
 	description: 'Play a game of trivia! ❔',
 	usage: '[difficulty]',
 	cooldown: 5,
 	category: 'games',
+	options: {
+		difficulty: {
+			type: 'String',
+			description: 'The difficulty of the game',
+			choices: difficulties.reduce((obj, d) => ({ ...obj, [d[0].toUpperCase() + d.slice(1)]: d }), {}),
+		},
+		user: {
+			type: 'User',
+			description: 'The user you want to challenge',
+		},
+	},
 	execute(message, args) {
-		const { Trivia } = require('discord-gamecord');
-		const color = require('../../color.json');
-		const sendError = require('../../error.js');
-		const difficulties = [
-			'easy',
-			'medium',
-			'hard',
-		];
-
 		if (!args[0]) {return sendError('You need to specify a difficulty', message.channel);}
 
 		if (!difficulties.includes(args[0].toLowerCase())) {return sendError('Please provide a valid difficulty', message.channel);}
 
 		new Trivia({
+			...gameOptions,
 			message: message,
 			opponent: message.mentions.users.first(),
-			embed: {
-				title: 'Trivia',
-				description: '🕓 You have {time} seconds to respond!',
-				color: color.bot_theme,
-			},
-			time: 60000,
 			difficulty: args[0],
-			buttons: {
-				one: '1️⃣',
-				two: '2️⃣',
-				three: '3️⃣',
-				four: '4️⃣',
+		}).startGame();
+	},
+	async executeSlash(interaction) {
+		const difficulty = interaction.options.getString('difficulty', true);
+		if (!difficulties.includes(difficulty)) {return interaction.reply('Please provide a valid difficulty');}
+		const user = interaction.options.getUser('user', true);
+		await interaction.deferReply();
+		const message = {
+			author: interaction.user,
+			channel: {
+				async send(option) {
+					return await interaction.editReply(option);
+				},
 			},
-			winMessage: '<:check:807305471282249738> Your answer was correct! You answered **{answer}**',
-			loseMessage: '<:X_:807305490160943104> Your answer was Incorrect! The correct answer was **{answer}**',
-			othersMessage: '<:X_:807305490160943104> You are not allowed to use buttons for this message!',
+		};
+		new Trivia({
+			...gameOptions,
+			message,
+			opponent: user,
+			difficulty: difficulty,
 		}).startGame();
 	},
 };
